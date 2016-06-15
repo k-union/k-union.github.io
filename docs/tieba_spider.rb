c7c2ae3b867dd07cc104e1e7debe1902cbe00b95
MopperWhite <mopperwhite@gmail.com>
2016-06-15 17:01:10 +0800

    DOCS

---
+++ docs/tieba_spider.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+#encoding=UTF-8
+require 'net/http'
+require 'cgi'
+require 'json'
+require 'yaml'
+require 'nokogiri'
+$header={
+        "Accept"=>"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Encoding"=>"deflate,sdch",
+        "Accept-Language"=>"zh-CN,zh;q=0.8",
+        "Connection"=>"keep-alive",
+        "User-Agent"=>"Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36",
+}
+$http=Net::HTTP.new "tieba.baidu.com"
+def get index
+        doc=Nokogiri::HTML.parse $http.get("http://tieba.baidu.com/f?kw=%E8%BD%AC%E5%9F%BA%E5%9B%A0&ie=utf-8&tab=good&cid=&pn=#{index*50}", $header).body
+        flag=doc.at("//code[@id='pagelet_html_frs-list/pagelet/thread_list']/comment()")
+        doc=Nokogiri::HTML.parse flag.text unless flag.nil?
+        doc.xpath(".//li[contains(@class,'j_thread_list') and @data-field]").map do |li|
+                data=JSON.load li["data-field"].gsub('&quot;','"')
+                username=data["author_name"]
+                kz=data["id"]
+                top= data["is_top"]==1
+                fine= data["is_good"]==1
+                live= data["is_portal"]==1
+                reply_num=data["reply_num"]
+                /^\s*(.+?)\s*$/===li.at(".//div[contains(@class,'threadlist_title')]").text
+                title=$1
+                sn=li.at(".//div[contains(@class,'threadlist_abs')]")
+                if sn and /^\s+(.+?)\s+$/===sn.text
+                        summary=$1
+                else
+                        summary=""
+                end
+                {
+                        :title => title,
+                        :author => username,
+                        :summary => summary,
+                        :kz => kz,
+                        :rpn => reply_num
+                }
+        end
+end
+
+File.write "zjy_threads.json", (0..800/50).map{|i|
+        sleep rand 1..5
+        puts "GET PAGE #{i}"
+        get(i)
+}.flatten.to_json
